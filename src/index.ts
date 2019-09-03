4b84f16010135a0f6bd32793e0e4a4105c8d8640
Josh Asch <josh.asch@gmail.com>
2019-09-03 22:20:26 +0100

    Add a basic ping-pong script

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import Discord from 'discord.js';
+import { config as loadEnv } from 'dotenv';
+
+loadEnv();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
