73b9f98e420db6241eecbf6ef770deee7e826d09
Josh Asch <josh.asch@gmail.com>
2019-10-24 19:50:14 +0100

    Don’t download prompt list when adding
    Downloading the entire list just to show a count is a waste of bandwidth

--- src/commands/prompt.ts
+++ src/commands/prompt.ts
@@ -4,13 +4,15 @@ import { PREFIX } from '../constants';
 import { deleteAfterDelay } from '../messages';
 import { pluralise } from '../strings';
 
+const random = (max: number) => Math.floor(Math.random() * max);
+
 const promptCommand: CommandFn = async (params, msg) => {
   const serverId = msg.guild.id;
   const channel = msg.channel;
 
-  const serverPrompts = await DB.getArrayAtPath(`prompts/${serverId}`);
-
   if (params.length === 0) {
+    const serverPrompts = await DB.getArrayAtPath(`prompts/${serverId}`);
+
     if (!serverPrompts.length) {
       return channel.send("There aren't any prompts saved at the moment. 😔");
     }
@@ -23,21 +25,13 @@ const promptCommand: CommandFn = async (params, msg) => {
     );
     await DB.setPath(`prompts/${serverId}`, updatedPromptList);
 
-    const newCount = updatedPromptList.length;
-    const newCountMessage = `(${pluralise(newCount, 'prompt')} left)`;
-
-    return channel.send(
-      `You should draw... ${selectedPrompt}.\n${newCountMessage}`
-    );
+    return channel.send(`You should draw... ${selectedPrompt}.`);
   }
 
   const promptToAdd = params.join(' ');
   await DB.pushAtPath(`prompts/${serverId}`, promptToAdd);
 
-  const newCount = serverPrompts.length + 1;
-  const newCountMessage = `${pluralise(newCount, 'prompt')} saved.`;
-
-  const sentMessage = await channel.send(`✅ Got it! ${newCountMessage}`);
+  const sentMessage = await channel.send(`✅ Got it!`);
   deleteAfterDelay(msg, sentMessage);
 };
 
