af4fc5b7d3d6708034ec91d5e9745d87aa6bd18e
Josh Asch <josh.asch@gmail.com>
2019-10-31 22:04:06 +0000

    Allow “hugs” and hug everyone if no target is specified

--- src/commands/hug.ts
+++ src/commands/hug.ts
@@ -2,10 +2,10 @@ import { Command, CommandFn } from '../interfaces';
 import { PREFIX } from '../constants';
 
 const sendHugs: CommandFn = (params, msg) => {
+  const target = params.length ? params.join(' ') : 'everyone';
+
   msg.channel.send(
-    `*hugs ${params.join(
-      ' '
-    )}* :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart:`
+    `*hugs ${target}* :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart:`
   );
   msg.delete();
 };
@@ -13,5 +13,6 @@ const sendHugs: CommandFn = (params, msg) => {
 export const hug: Command = {
   description: `Sends hugs to someone, e.g. ${PREFIX}hugs valentine`,
   fn: sendHugs,
-  params: ['user']
+  params: ['user'],
+  aliases: ['hugs']
 };
