fc600a6560798a03898d7f26228c36ad14cfa5df
Josh Asch <josh.asch@gmail.com>
2019-10-31 22:00:06 +0000

    Delete source message when hugging

--- src/commands/hug.ts
+++ src/commands/hug.ts
@@ -2,11 +2,12 @@ import { Command, CommandFn } from '../interfaces';
 import { PREFIX } from '../constants';
 
 const sendHugs: CommandFn = (params, msg) => {
-  return msg.channel.send(
+  msg.channel.send(
     `*hugs ${params.join(
       ' '
     )}* :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart:`
   );
+  msg.delete();
 };
 
 export const hug: Command = {
