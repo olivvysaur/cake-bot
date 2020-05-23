78879283bfd3e52b5050eb4e9a5d05b392478990
Olivia Asch <olivia.c.asch@gmail.com>
2020-05-23 12:24:27 +0100

    Make edit interactive to make copy/paste easier

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -1,4 +1,4 @@
-import { TextChannel } from 'discord.js';
+import { TextChannel, Message } from 'discord.js';
 
 import { Command, CommandFn } from '../interfaces';
 import { deleteAfterDelay } from '../messages';
@@ -6,28 +6,51 @@ import { client } from '..';
 import { emoji } from '../emoji';
 
 const editMessage: CommandFn = async (params, msg) => {
-  if (params.length < 2) {
+  if (params.length < 1) {
     return;
   }
 
   const messageId = params[0];
-  const newContent = params.slice(1).join(' ');
 
   const serverChannels = msg.guild.channels;
   let success = false;
   let canEdit = true;
+  let cancelled = false;
   await Promise.all(
     serverChannels
       .filter((channel) => channel.type === 'text')
       .map(async (channel) => {
         try {
-          const message = await (channel as TextChannel).fetchMessage(
-            messageId
-          );
+          const textChannel = channel as TextChannel;
+          const message = await textChannel.fetchMessage(messageId);
           if (message) {
             if (message.author.id === client.user.id) {
-              message.edit(newContent);
               success = true;
+              await textChannel.send(`
+Editing message ${messageId}. Here is the existing content, in case you need to copy and paste:
+\`\`\`
+${message.content}
+\`\`\`
+Now what should the new content be? Reply with "cancel" to cancel.`);
+
+              const messageFilter = (message: Message) =>
+                message.author.id === msg.author.id;
+              const response = await textChannel.awaitMessages(messageFilter, {
+                time: 60000,
+                maxMatches: 1,
+              });
+              if (
+                !response ||
+                !response.size ||
+                response.first().content.toLowerCase() === 'cancel'
+              ) {
+                cancelled = true;
+                return textChannel.send(`${emoji.error} Edit cancelled.`);
+              }
+
+              const newContent = response.first().content;
+              await message.edit(newContent);
+              await response.first().react(emoji.success);
             } else {
               canEdit = false;
             }
@@ -38,16 +61,14 @@ const editMessage: CommandFn = async (params, msg) => {
       })
   );
 
-  if (success) {
-    await msg.react(emoji.success);
-    deleteAfterDelay(msg);
-  } else {
-    const error = canEdit
-      ? "I couldn't find that message."
-      : "I can't edit messages I didn't send.";
-    const sentMessage = await msg.channel.send(`${emoji.error} ${error}`);
-    deleteAfterDelay(msg, sentMessage);
+  if (success || cancelled) {
+    return;
   }
+
+  const error = canEdit
+    ? "I couldn't find that message."
+    : "I can't edit messages I didn't send.";
+  return msg.channel.send(`${emoji.error} ${error}`);
 };
 
 export const edit: Command = {
