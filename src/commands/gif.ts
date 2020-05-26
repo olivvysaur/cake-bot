377bf2a327342e948fb32a30d7157e70bd53091a
Olivia Asch <olivia.c.asch@gmail.com>
2020-05-26 19:37:38 +0100

    Switch to tenor for gif search

--- src/commands/gif.ts
+++ src/commands/gif.ts
@@ -3,13 +3,22 @@ import axios from 'axios';
 
 import { Command, CommandFn } from '../interfaces';
 import { emoji } from '../emoji';
+import { random } from '../random';
 
-const GIPHY_RANDOM_URL = 'https://api.giphy.com/v1/gifs/random';
-const GIPHY_TRANSLATE_URL = 'https://api.giphy.com/v1/gifs/translate';
+const RANDOM_URL = 'https://api.tenor.com/v1/random';
+const TRENDING_URL = 'https://api.tenor.com/v1/trending';
+
+const baseParams = {
+  key: process.env.TENOR_API_KEY,
+  locale: 'en_US',
+  media_filter: 'minimal',
+  contentfilter: 'low',
+  limit: 50,
+};
 
 const getGif: CommandFn = async (params, msg) => {
-  if (!process.env.GIPHY_API_KEY) {
-    console.error('gif command will not work because GIPHY_API_KEY is not set');
+  if (!process.env.TENOR_API_KEY) {
+    console.error('gif command will not work because TENOR_API_KEY is not set');
     return;
   }
 
@@ -17,14 +26,14 @@ const getGif: CommandFn = async (params, msg) => {
 
   if (params.length === 0) {
     const queryParams = {
-      api_key: process.env.GIPHY_API_KEY,
-      tag: '',
-      rating: 'PG-13',
+      ...baseParams,
     };
+
     const query = querystring.stringify(queryParams);
-    const url = `${GIPHY_RANDOM_URL}?${query}`;
+    const url = `${TRENDING_URL}?${query}`;
 
     const response = await axios.get(url);
+
     if (response.status !== 200) {
       console.error(`gif command failed due to error ${response.status}`);
       return channel.send(
@@ -32,33 +41,45 @@ const getGif: CommandFn = async (params, msg) => {
       );
     }
 
-    return channel.send(response.data.data.image_url);
-  } else {
-    const searchTerm = params.join(' ');
-    const queryParams = {
-      api_key: process.env.GIPHY_API_KEY,
-      s: searchTerm,
-      rating: 'PG-13',
-    };
-    const query = querystring.stringify(queryParams);
-    const url = `${GIPHY_TRANSLATE_URL}?${query}`;
+    const results = response.data.results;
 
-    const response = await axios.get(url);
-    if (response.status !== 200) {
-      console.error(`gif command failed due to error ${response.status}`);
+    if (!results.length) {
       return channel.send(
         `${emoji.error} I'm having trouble finding GIFs at the moment.`
       );
     }
 
-    if (!response.data.data?.images?.original?.url) {
-      return channel.send(
-        `${emoji.error} I didn't find any good GIFs for that search.`
-      );
-    }
-
-    return channel.send(response.data.data.images.original.url);
+    const chosenIndex = random(results.length);
+    return channel.send(results[chosenIndex].itemurl);
   }
+
+  const searchTerm = params.join(' ');
+  const queryParams = {
+    ...baseParams,
+    q: searchTerm,
+  };
+
+  const query = querystring.stringify(queryParams);
+  const url = `${RANDOM_URL}?${query}`;
+
+  const response = await axios.get(url);
+  if (response.status !== 200) {
+    console.error(`gif command failed due to error ${response.status}`);
+    return channel.send(
+      `${emoji.error} I'm having trouble finding GIFs at the moment.`
+    );
+  }
+
+  const results = response.data.results;
+
+  if (!results.length) {
+    return channel.send(
+      `${emoji.error} I'm having trouble finding GIFs at the moment.`
+    );
+  }
+
+  const chosenIndex = random(results.length);
+  return channel.send(results[chosenIndex].itemurl);
 };
 
 export const gif: Command = {
