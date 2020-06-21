29d31bb3cce0c3906aacc752ef6bc3a632b06493
Olivia Asch <olivia.c.asch@gmail.com>
2020-06-21 12:29:39 +0100

    Make minesweeper 8x8 instead of 8x6

--- src/commands/minesweeper/constants.ts
+++ src/commands/minesweeper/constants.ts
@@ -1,8 +1,8 @@
 export const WIDTH = 8;
-export const HEIGHT = 6;
+export const HEIGHT = 8;
 export const MINES = 8;
 
-export const COVERED = '🟪';
+export const COVERED = '';
 export const MINE = '💥';
 export const FLAG = '🚩';
 export const NUMBERS = ['🟦', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣'];
